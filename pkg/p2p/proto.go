1caaa2a7381afc34ac2206c6d2eddeecdf7813f4
sinea <bryan.fury.snt@gmail.com>
2019-05-08 10:30:56 +0300

    Named return values

--- pkg/p2p/proto.go
+++ pkg/p2p/proto.go
@@ -16,54 +16,58 @@ type Proto struct {
 	buffer     []byte
 }
 
-func (p *Proto) Write(command uint8, body []byte) error {
+func (p *Proto) Write(command uint8, body []byte) (err error) {
 	message := make([]byte, 6)
 	message[0] = header
 	message[1] = command
-	binary.BigEndian.PutUint32(message[2:6], uint32(len(body)))
-	message = append(message, body...)
+
+	if body != nil {
+		binary.BigEndian.PutUint32(message[2:6], uint32(len(body)))
+		message = append(message, body...)
+	}
 
 	n, err := p.connection.Write(message)
 
 	if err != nil {
-		return err
+		return
 	}
 
 	if n != len(message) {
-		return errors.New("wrote incomplete message")
+		err = errors.New("wrote incomplete message")
 	}
 
-	return nil
+	return
 }
 
-func (p *Proto) Read() (uint8, []byte, error) {
+func (p *Proto) Read() (command uint8, payload []byte, err error) {
 	buffer := make([]byte, bufferSize)
 	n, err := p.connection.Read(buffer)
 
 	if err != nil {
-		return 0, nil, err
+		return
 	}
 
 	p.buffer = append(p.buffer, buffer[:n]...)
 
 	if len(p.buffer) < 8 {
-		return 0, nil, nil
+		return
 	}
 
 	if p.buffer[0] != header {
-		return 0, nil, errors.New("invalid header")
+		err = errors.New("invalid header")
+		return
 	}
 
 	payloadLength := binary.BigEndian.Uint32(p.buffer[2:6])
 	if uint32(len(p.buffer)) < payloadLength+6 {
-		return 0, nil, nil
+		return
 	}
 
-	command := p.buffer[1]
-	payload := p.buffer[6 : 6+payloadLength]
+	command = p.buffer[1]
+	payload = p.buffer[6 : 6+payloadLength]
 
 	// Trim the buffer
 	p.buffer = p.buffer[6+payloadLength:]
 
-	return command, payload, nil
+	return
 }
